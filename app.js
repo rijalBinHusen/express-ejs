e581f7409ff9ae0c3fa8df6e85be5c9bb25775b8
rijal <jeryzal333@gmail.com>
2021-10-24 06:51:22 +0700

    add nodemon and ejs engine

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-app.get("/", (req, res) => res.send("Hello world"))
+app.set("views", "./views") //views directory
+app.set("view engine", "ejs") //template engine register
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/:name", (req, res) => res.send(`Nama saya: ${req.params.name}`))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
